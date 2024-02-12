e8c9fb0fa24036fffed2042269bfd6f6e9ea9247
Atila Neves <atila.neves@gmail.com>
2024-02-12 18:12:14 +0100

    Improvements

---
+++ README.md
@@ -0,0 +1,1 @@
+Caution: run this in a container! The app will run random code from the internet.
--- app.d
+++ app.d
@@ -16,20 +16,34 @@ void run(O)(auto ref O output) {
     import std.datetime.stopwatch: StopWatch, AutoStart;
     import std.algorithm: filter;
     import std.range: walkLength;
+    import std.array: array;
 
     output.writeln("Checking packages...\n");
     auto sw = StopWatch(AutoStart.yes);
+    auto packages = getPackages.array;
 
-    auto packages = getPackages;
-    auto okPackages = packages
-        .save
-        .parallelMap!builds
-        .filter!(x => x != "");
+    check!builds(output, packages, "dub-build.txt");
+    check!tests(output, packages, "dub-test.txt");
+}
 
-    output.writeln("\n\nChecked ", packages.length, " packages in ", sw.peek);
-    output.writeln(okPackages.save.walkLength, " packages still build.\n\n");
+void check(alias F, O)(auto ref O output, in string[] dubPackages, in string outputFileName) {
+    import std.datetime.stopwatch: StopWatch, AutoStart;
+    import std.array: array;
+    import std.algorithm: filter;
+    import std.array: array;
 
-    write(okPackages);
+    auto sw = StopWatch(AutoStart.yes);
+
+    auto okPackages = dubPackages
+        .parallelMap!F
+        .filter!(x => x != "")
+        .array;
+
+    output.writeln("\n\nChecked ", dubPackages.length, " packages in ", sw.peek);
+    output.writeln("Outputting to ", outputFileName);
+    output.writeln(okPackages.length, " packages still build.\n\n");
+    write(outputFileName, okPackages);
+
 }
 
 auto getPackages() {
@@ -37,11 +51,13 @@ auto getPackages() {
     import std.json: parseJSON;
     import std.algorithm: map;
 
+    import std.range;
     return "https://code.dlang.org/packages/index.json"
         .get
         .parseJSON
         .array // the JSONValue one, not std.array.array
         .map!(a => a.str)
+        .take(250);
         ;
 }
 
@@ -58,21 +74,29 @@ auto parallelMap(alias F, R)(R range) {
 }
 
 string builds(in string dubPackage) {
+    return checkBuild(dubPackage, ["dub", "build", "-y"]);
+}
+
+string tests(in string dubPackage) {
+    return checkBuild(dubPackage, ["dub", "test"]);
+}
+
+string checkBuild(in string dubPackage, in string[] cmd) {
     import std.process: execute;
     import std.stdio: writeln;
     writeln("Checking ", dubPackage);
-    auto ret = execute(["dub", "build", "-y", dubPackage]);
+    auto ret = execute(cmd ~ dubPackage);
     return ret.status == 0
         ? dubPackage
         : "";
 }
 
-void write(R)(R okPackages) {
+void write(R)(in string fileName, R okPackages) {
     import std.stdio: File;
     import std.array: array;
     import std.algorithm: sort;
 
-    auto file = File("packages.txt", "w");
+    auto file = File(fileName, "w");
     foreach(pkg; sort(okPackages.array)) {
         file.writeln(pkg);
     }
