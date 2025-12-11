9838fa7ce616c045eada44e674aa9c3527577f6c
Atila Neves <atila.neves@gmail.com>
2025-12-11 14:52:38 +0100

    Fork repo if not already in `forks`

--- build_fork.sh
+++ build_fork.sh
@@ -10,6 +10,10 @@ repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 fork_dir="$repo_root/forks/$1"
 
 if [[ ! -d "$fork_dir" ]]; then
+    "$repo_root"/fork_dub_package "$1"
+fi
+
+if [[ ! -d "$fork_dir" ]]; then
     echo "Fork not found: $fork_dir" >&2
     exit 1
 fi
